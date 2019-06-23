b84e88aa716bc6daa66a4289308ebc15fec1385c
unknown <aurelie.benovici@gmail.com>
2019-06-24 00:25:53 +0200

    small change

--- routes/meetup-routes.js
+++ routes/meetup-routes.js
@@ -58,8 +58,9 @@ router.get("/add-meetup", async (req, res, next) => {
 // Create a meetup when the login form is submitted
 router.post("/add-meetup", async (req, res, next) => {
   const { name, description, eventDate, group } = req.body;
+
   const createBy = req.user._id;
-  // const group = req.body.group;
+
   Meetup.create({
     name,
     description,
@@ -101,7 +102,7 @@ router.post("/add-meetup", async (req, res, next) => {
             });
           });
 
-          // res.json({ meetup: userUpdate, group: groupDoc });
+          res.json({ meetup: userUpdate, group: groupDoc });
         });
       });
     })
@@ -112,6 +113,7 @@ router.post("/add-meetup", async (req, res, next) => {
 router.get("/meetup/:meetupId", (req, res, next) => {
   const { meetupId } = req.params;
   const user = req.user._id;
+  // console.log(req.user._id, " userId meetupDetails");
   Meetup.findById(meetupId)
     .populate("GotoEvent", "fullName")
 
@@ -125,23 +127,24 @@ router.get("/meetup/:meetupId", (req, res, next) => {
           console.log("*********UserDoc***********");
           console.log(userInfo);
           console.log("*********MeetupDoc***********");
-          let newArr = MeetupDoc.GotoEvent.map(i => {
-            console.log(
-              i._id.toString(),
-              " + = ",
-              user.toString()
-            );
-            // return i._id.indexOf(user) > 0 ? "yes" : "no";
-            return i._id.toString() === user.toString()
-              ? "yes"
-              : "no";
-          });
-          console.log(newArr);
+          let checkMember = [];
+          function checkMe() {
+            MeetupDoc.GotoEvent.map(i => {
+              i._id.toString() === user.toString()
+                ? checkMember.push(i)
+                : "no";
+
+              return checkMember;
+            });
+          }
+          checkMe();
+          console.log(checkMember);
+
           res.json({
             meetupInf: MeetupDoc,
             userInf: userInfo,
             groupInf: groupDoc,
-            test: [newArr]
+            test: [checkMember]
           });
         });
       });
@@ -174,18 +177,20 @@ router.get("/my-meetup", (req, res, next) => {
 });
 
 router.post("/process-goTo", (req, res, next) => {
-  const { post, liker } = req.body;
+  const { event, user } = req.body;
+  const eventId = event.map(i => i._id);
   User.findByIdAndUpdate(
-    liker,
+    user,
     {
-      $push: { eventToGo: post._id }
+      $push: { eventToGo: eventId }
     },
     { runValidators: true, new: true }
   )
     .then(response => {
       response.encryptedPassword = undefined;
+      // console.log(event._id);
       Meetup.findByIdAndUpdate(
-        post._id,
+        eventId,
         {
           $push: { GotoEvent: response._id }
         },
@@ -195,202 +200,20 @@ router.post("/process-goTo", (req, res, next) => {
       });
     })
     .catch(err => next(err));
-  // GotoEvent meetup model
-  // eventToGo user model
+  //  GotoEvent meetup model
+  //  eventToGo user model
 });
 
-// router.get("/search/:where", (req,res,next)=>{
-//   const {where} = req.params
-//   console.log(req.params)
-//   House.find({"city" : {$eq : where}})
-//   .then(houseDoc => res.json(houseDoc))
-//   .catch(err=>next(err))
-// })
+router.post("/edit-meetup", (req, res, next) => {
+  const { newName, description, eventId } = req.body;
+  console.log(newName, "", eventId);
 
-// router.post("/search", (req, res, next) => {
-//   const { arrayOfDates, where, guest } = req.body;
-//   House.find({
-//     city: { $eq: where },
-//     availableDates: { $all: arrayOfDates },
-//     accommodates: { $gte: guest }
-//   })
-//     .then(houseDoc => {
-//       res.json(houseDoc);
-//     })
-//     .catch(err => next(err));
-// });
-
-// Get the houses that matches the user email then render it in the UserHouses component
-// router.get("/userhouses", (req, res, next) => {
-//   House.find({ owner: { $eq: req.user._id } })
-//     .then(houseDoc => res.json(houseDoc))
-//     .catch(err => next(err));
-// });
-
-// Delete a house
-// router.delete("/deletehouse/:id", (req, res, next) => {
-//   const { id } = req.params;
-
-//   House.findByIdAndRemove(id)
-//     .then(houseDoc => {
-//       res.send(houseDoc);
-//     })
-//     .catch(err => next(err));
-// });
-
-// Edit a house - Form in the EditPlace component
-// router.put("/houses/:id", (req, res, next) => {
-//   const { id } = req.params;
-//   const {
-//     property_type,
-//     room_type,
-//     accommodates,
-//     beds,
-//     bedrooms,
-//     bathrooms,
-//     neighbourhood,
-//     amenities,
-//     name,
-//     description,
-//     country,
-//     city,
-//     price,
-//     xl_picture_url,
-//     xl_picture_url_2,
-//     xl_picture_url_3
-//   } = req.body;
-
-//   House.findByIdAndUpdate(
-//     id,
-//     {
-
-//       $set: {
-//         property_type,
-//         room_type,
-//         accommodates,
-//         beds,
-//         bedrooms,
-//         bathrooms,
-//         neighbourhood,
-//         amenities,
-//         name,
-//         description,
-//         country,
-//         city,
-//         price,
-//         xl_picture_url,
-//         xl_picture_url_2,
-//         xl_picture_url_3
-//       }
-//     },
-//     { runValidators: true, new: true }
-//   )
-//     .then(houseDoc => {
-//       res.send(houseDoc);
-//     })
-//     .catch(err => {
-//       console.log("Something went wrong", err);
-//     });
-// });
-
-// router.post("/booking/:houseId", (req, res, next) => {
-//   const { arrayOfDates, where, guests, price, currentUser } = req.body;
-//   const { houseId } = req.params;
-//   Booking.create({ arrayOfDates, guests, houseId, price, currentUser })
-//     .then(userDoc => {
-//       res.json(userDoc);
-//     })
-//     .catch(err => next(err));
-// });
-
-// router.get("/bookings-list", (req, res, next) => {
-//   Booking.find({ currentUser: { $eq: req.user._id } })
-//     .populate("houseId")
-//     .sort({ createdAt: -1 })
-//     .then(currentUser => res.json(currentUser))
-//     .catch(err => next(err));
-// });
+  Meetup.findByIdAndUpdate(eventId, {
+    $set: { name: newName, description: description }
+  }).then(newEvent => {
+    console.log("******New Event ********");
+    console.log(newEvent);
+  });
+});
 
 module.exports = router;
-
-//*******************************************************************
-//***********************************************************************//
-// const express = require("express");
-// const mongoose = require("mongoose");
-
-// const Meetup = require("../models/meetup-model");
-
-// const meetupRouter = express.Router();
-
-// meetupRouter.post("/meetup", (req, res, next) => {
-//   const { title, description } = req.body;
-//   // const owner = res.user._id;
-
-//   Meetup.create({
-//     title,
-//     description
-//     // , owner
-//   })
-//     .then(response => {
-//       res.status(200).json(response);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// meetupRouter.get("/meetup", (req, res, next) => {
-//   Meetup.find()
-//     .populate("group")
-//     .then(allMeetups => {
-//       res.json(allMeetups);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// meetupRouter.get("/meetup/:meetupId", (req, res, next) => {
-//   const { meetupId } = req.params;
-//   Meetup.findById(meetupId)
-//     .then(meeDoc => res.json(meeDoc))
-//     .catch(err => next(err));
-// });
-//
-
-// // Put route => update a specifiq item
-// meetupRouter.put("/meetups/:id", (req, res, next) => {
-//   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-//     res.status(400).json({ message: "Specified id is not valid" });
-//     return;
-//   }
-
-//   Meetup.findByIdAndUpdate(req.params.id, req.body)
-//     .then(() => {
-//       res.json({
-//         message: `Meetup with ${req.params.id} is updated successfully.`
-//       });
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// // DELETE route => to delete a specific meetup
-// meetupRouter.delete("/meetups/:id", (req, res, nex) => {
-//   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-//     res.status(400).json({ message: "Specified id is not valid" });
-//     return;
-//   }
-//   Meetup.findOneAndRemove(req.params.id)
-//     .then(() => {
-//       res.json({
-//         message: `Meetup with ${req.params.id} is removed successfully.`
-//       });
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// module.exports = meetupRouter;
